0e431b3ae699cd4b38c669f1273f16f883245259
Radu <radu.alex19@yahoo.com>
2022-12-22 20:31:24 +0200

    GoodOne

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,20 +7,15 @@ import org.example.Util.SessionManager;
 
 import java.sql.Date;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Scanner;
 
+import static com.mysql.cj.protocol.a.MysqlTextValueDecoder.getDate;
 import static java.lang.System.exit;
 
 
 public class Main {
-    public static void printMenu(String[] options) {
-        for (String option : options) {
-            System.out.println(option);
-        }
-        System.out.println("Choose your option: ");
-    }
-
     public static void main(String[] args) {
 
         CarRepository carRepository = new CarRepositoryImpl();
@@ -31,6 +26,111 @@ public class Main {
         RefundRepository refundRepository = new RefundRepositoryImpl();
         ReservationRepository reservationRepository = new ReservationRepositoryImpl();
 
+        initRepos(carRepository,
+                  branchRepository,
+                  customerRepository,
+                  employeeRepository,
+                  loanRepository,
+                  refundRepository,
+                  reservationRepository);
+
+        String[] options = {
+                "1- Create Customer",
+                "2- Create branch",
+                "3- Create car",
+                "4- Create reservation",
+                "5- Exit"
+        };
+
+        Scanner scanner = new Scanner(System.in);
+        printMenu(options);
+        int option = 1;
+        while (option != 5) {
+
+            try {
+                option = Integer.valueOf(scanner.nextLine());
+                switch (option) {
+                    case 1:
+                        customerRepository.create(new Customer(getString("Input first name: ", scanner),
+                                getString("Input last name: ", scanner),
+                                getString("Input email: ", scanner),
+                                getString("Input address: ", scanner)));
+
+                        outputSuccessMsgAndDisplayMenu("Customer added", options);
+                        customerRepository.create(new Customer());
+
+                        break;
+                    case 2:
+                        branchRepository.create(new Branch(getString("Input address: ", scanner),
+                                                            getString("Input city: ", scanner)));
+                        outputSuccessMsgAndDisplayMenu("Branch added", options);
+                        break;
+                    case 3:
+                        carRepository.create(new Cars(
+                                getString("Input brand: ", scanner),
+                                getString("Input model: ", scanner),
+                                getString("Input: bodytype", scanner),
+                                getInteger("Input year: ", scanner),
+                                getString("Input colour: ", scanner),
+                                getInteger("Input mileage: ", scanner),
+                                getInteger("Input amount: ", scanner),
+                                status("Input status", scanner)
+                        ));
+                        outputSuccessMsgAndDisplayMenu("Car added", options);
+                        carRepository.create(new Cars());
+                        break;
+                    case 4:
+                        reservationRepository.create(new Reservation());
+
+                        outputSuccessMsgAndDisplayMenu("Reservation added", options);
+                        reservationRepository.create(new Reservation());
+                        break;
+                    case 5:
+                        exit(0);
+                }
+            } catch (Exception e) {
+                scanner.next();
+            }
+        }
+    }
+
+    private static void outputSuccessMsgAndDisplayMenu(String message, String[] options) {
+        System.out.println(message);
+        printMenu(options);
+    }
+
+    private static String getString(String x, Scanner scanner) {
+        System.out.println(x);
+        String stringValue = scanner.nextLine();
+        return stringValue;
+    }
+    private static Integer getInteger(String x, Scanner scanner) {
+        System.out.println(x);
+        Integer integerValue = Integer.valueOf(scanner.nextLine());
+        return integerValue;
+    }
+    private static Status status(String x, Scanner scanner) {
+        System.out.println(x);
+        Status statusValue = Status.valueOf(scanner.nextLine());
+        return statusValue;
+    }
+    private static Date date(Long x, Scanner scanner) {
+        System.out.println(x);
+        Date dateValue = Date.valueOf("yyyy-mm-dd");
+        return dateValue;
+    }
+
+
+
+
+    public static void printMenu(String[] options) {
+        for (String option : options) {
+            System.out.println(option);
+        }
+        System.out.println("Choose your option: ");
+    }
+
+    private static void initRepos(CarRepository carRepository, BranchRepository branchRepository, CustomerRepository customerRepository, EmployeeRepository employeeRepository, LoanRepository loanRepository, RefundRepository refundRepository, ReservationRepository reservationRepository) {
         Branch mainBranch = new Branch("Burebista 69", "Cluj-Napoca");
 
         branchRepository.create(mainBranch);
@@ -48,7 +148,7 @@ public class Main {
         );
         employeeRepository.create(employee2);
 
-        System.out.println(carRepository.findAll());
+        //System.out.println(carRepository.findAll());
         Branch branch1 = new Branch(
                 "Aurel Vlaicu 3",
                 "Bucuresti"
@@ -82,7 +182,7 @@ public class Main {
         employeeRepository.create(employee1);
 
 
-        System.out.println(employeeRepository.findAll());
+       // System.out.println(employeeRepository.findAll());
 
         Refund refund1 = new Refund(
                 employee1,
@@ -91,7 +191,7 @@ public class Main {
                 "Bad experience"
         );
         refundRepository.create(refund1);
-        System.out.println(refundRepository.findAll());
+        //System.out.println(refundRepository.findAll());
 
         Loan loan1 = new Loan(
                 employee1,
@@ -99,7 +199,7 @@ public class Main {
 
         );
         loanRepository.create(loan1);
-        System.out.println(loanRepository.findAll());
+       // System.out.println(loanRepository.findAll());
 
         Customer customer1 = new Customer(
                 "Violeta",
@@ -108,7 +208,7 @@ public class Main {
                 "Craiova 28-30, Cluj"
         );
         customerRepository.create(customer1);
-        System.out.println(customerRepository.findAll());
+       // System.out.println(customerRepository.findAll());
 
 
         branchRepository.create(branch1);
@@ -118,7 +218,7 @@ public class Main {
                 "Pitesti"
         );
         branchRepository.create(branch2);
-        System.out.println(branchRepository.findAll());
+       // System.out.println(branchRepository.findAll());
 
         Reservation reservation1 = new Reservation(
                 new Date(Instant.now().toEpochMilli()),
@@ -130,52 +230,8 @@ public class Main {
                 branch2
         );
         reservationRepository.create(reservation1);
-        System.out.println(reservationRepository.findAll());
+        //System.out.println(reservationRepository.findAll());
 
         SessionManager.shutdown();
-
-
-        String[] options = {
-                "1- Create Customer",
-                "2- Create branch",
-                "3- Create car",
-                "4- Create reservation",
-                "5- Exit"
-        };
-        Scanner scanner = new Scanner(System.in);
-        int option = 1;
-        while (option != 5) {
-            printMenu(options);
-            try {
-                option = scanner.nextInt();
-                switch (option) {
-                    case 1:
-                        customerRepository.create(new Customer(
-                                "Violeta",
-                                "Popescu",
-                                "violeta.popescu44@gmail.com",
-                                "Craiova 28-30, Cluj"));
-                        break;
-                    case 2:
-                        // get input params from console address = inputConsola
-                        // city = inputConsola
-                        // new Branch(address, city)
-
-                        branchRepository.create(new Branch());
-                        break;
-                    case 3:
-                        carRepository.create(new Cars());
-                        break;
-                    case 4:
-                        reservationRepository.create(new Reservation());
-                        break;
-                    case 5:
-                        exit(0);
-                }
-            } catch (Exception e) {
-                scanner.next();
-            }
-        }
     }
-
 }
